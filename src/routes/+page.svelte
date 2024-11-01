883b796b27fef3dc476db9e6c0e5bd00fe8170ce
Eureka Chen <eureka@ee-studio.com>
2024-11-01 11:51:18 +0800

    修改了头部信息

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,41 @@
-
-    <!-- 
-
-      ## Getting Started with LittleLink
-
-      This page has been built with every pre-designed button available in LittleLink by default. You can rearrange and delete as needed.
-
-      You can add your own brand or others brands you may need in the `css/brands.css` file. 
-
-      You can add custom icons to `images/icons/...`. It is recommended to use a 24x24 .SVG.
-
-      Edit the "Your Image Here" section to add your own personal branding, like a picture of yourself or your brand logo!
-
-      Edit the "Title" section to change the page heading. You can use something like your name, your social handle, or your brand name.
-
-      Edit the "Short Bio" section tell users about yourself or your brand.
-
-    -->
-
     <!-- Your Image Here -->
-    <img src="images/littlelink.svg" class="avatar" srcset="images/littlelink.svg 2x" alt="">
+    <img src="images/linktree-ar.svg" class="avatar" srcset="images/linktree-ar.svg 2x" alt="linktree AR">
 
     <!-- Title -->
-    <h1 role="heading">LittleLink</h1>
+    <h1 role="heading">Link Tree AR</h1>
 
     <!-- Short Bio -->
-    <p>An open source DIY Linktree alternative.</p>
+    <p>You can directly edit this page and permanently publish it to Arweave, then accessing your page through the ar.io gateway</p>
 
     <!--
 
       ## Breaking down <a> attributes:
       
-      1.) class="button button-default" | The first "button" here is telling this <a> tag that it should make this element a button and applies the default styling in `css/brands.css`.
+      1.) class="button button-default" | The first "button" here is telling this <a> tag that it should make this element a button and applies the default styling in `css/brands.css`.这里的第一个“按钮”告诉这个<a>标签，它应该使这个元素成为一个按钮，并在`css/brands.css`中应用默认样式。
       The second portion, button-default, is declaring the specific brand style you would like to apply. Here we're applying the "default" style and color.
+      第二部分，按钮默认值，声明您要应用的特定品牌风格。在这里，我们应用“默认”样式和颜色。
       If you want to make this button to use the brand colors for Discord, just change "button-default" to "button-discord". Brands are found in `css/brands.css`. You can always edit & add your own there.
+      如果你想让这个按钮使用Discord的品牌颜色，只需将“button-default”更改为“button-discord”。品牌可以在`css/Brands.css`中找到。您始终可以在那里编辑和添加自己的。
 
       2.) Replace the # in href="#" with the desired target URL for the button.
 
       3.) target="_blank" | This attribute opens links in a new tab. Remove this attribute to prevent links from opening in a new tab.
 
-      4.) rel="noopener" | This attribute instructs the browser to navigate to the target resource without granting the new browsing context access to the document that opened it.
+      4.) rel="noopener" | This attribute instructs the browser to navigate to the target resource without granting the new browsing context access to the document that opened it.此属性指示浏览器导航到目标资源，而不授予新的浏览上下文访问打开它的文档的权限。
       This is especially useful when opening untrusted links. https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noopener
 
-      5.) role="button" | The button role identifies an element as a button to assistive technology such as screen readers.
+      5.) role="button" | The button role identifies an element as a button to assistive technology such as screen readers.按钮角色将元素标识为屏幕阅读器等辅助技术的按钮。
 
       ## Breaking down the <img> attributes:
       
-      1.) class="icon" | This class is telling the <img> tag that it should use the styling for icons found in `css/brands.css`.
+      1.) class="icon" | This class is telling the <img> tag that it should use the styling for icons found in `css/brands.css`.此类告诉<img>标签，它应该使用`css/brands.css`中的图标样式。
 
-      2.) src="images/icons/[icon-name].svg" | This defines the icon you would like to display from the 'images/icons/' folder. For example, you can change this to src="images/icons/discord.svg" to use the Discord icon.
-      Add your own 24x24 icons to the "icons" folder to reference them. We recommend providing a SVG.
+      2.) src="images/icons/[icon-name].svg" | This defines the icon you would like to display from the 'images/icons/' folder. For example, you can change this to src="images/icons/discord.svg" to use the Discord icon.这定义了您要在“images/icons/”文件夹中显示的图标。例如，您可以将其更改为src=“images/icons/discord.svg”以使用discord图标。
+      Add your own 24x24 icons to the "icons" folder to reference them. We recommend providing a SVG.将您自己的24x24图标添加到“icons”文件夹中以引用它们。我们建议提供SVG。
 
       3.) alt="" | Since the text at the end of the snippet, "..>[Button Text]</a><br>", explains what the button is, we use "alt=""" to nullify the icon annoucement from the accessibility tree. 
       This can improve the experience for assistive technology users by hiding what is essentially duplicated
+      这可以通过隐藏本质上重复的内容来改善辅助技术用户的体验
 
     -->
 
