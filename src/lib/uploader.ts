76440cc62643c44e9924e378900166896c009b25
Eureka Chen <eureka@ee-studio.com>
2024-11-03 11:04:12 +0800

    构建html

--- src/lib/uploader.ts
+++ src/lib/uploader.ts
@@ -4,24 +4,13 @@ import Arweave from 'arweave';
 import { Readable } from 'stream';
 
 export async function publish() {
-	/**
-	 * 从arweave钱包生成 key.
-	 */
+	
 	const arweave = new Arweave({});
-	const jwk = await arweave.wallets.generate();
-	/**
-	 * 使用arweave key 创建授权的turbo client
-	 */
-	const turboAuthClient = TurboFactory.authenticated({
-		privateKey: jwk,
-		...developmentTurboConfiguration
-	});
-
-	/**
-	 * 从私钥获取余额.
-	 */
-	const balance = await turboAuthClient.getBalance();
-	console.log('Balance:', balance);
+	const jwk = await arweave.wallets.generate();	
+	const turboAuthClient = TurboFactory.authenticated({privateKey: jwk,...developmentTurboConfiguration});	
+	
+    //const balance = await turboAuthClient.getBalance();
+	//console.log('Balance:', balance);
 
 	/**
 	 * Fetch the estimated amount of winc returned for 10 USD (1000 cents).
@@ -50,9 +39,7 @@ export async function publish() {
 			return readable;
 		},
 		fileSizeFactory: () => fileSize,
-		//contentType:'text/html',
-
-		dataItemOpts: {
+        dataItemOpts: {
 			// optional
 			tags: [
 				{
@@ -62,39 +49,37 @@ export async function publish() {
 			]
 			// no timeout or AbortSignal provided
 		},
-
 		signal: AbortSignal.timeout(10_000) // cancel the upload after 10 seconds
 	});
+
+    console.log("返回结果：", uploadResult);
 	console.log(JSON.stringify(uploadResult, null, 2));
 
-	/**
-	 * Tops up a wallet with Credits using tokens.
-	 * Default token is AR, using Winston as the unit.
-	 */
-	/* const topUpResult = await turboAuthClient.topUpWithTokens({
-          tokenAmount: WinstonToTokenAmount(100_000_000) // 0.0001 AR
-      });
-      console.log(JSON.stringify(topUpResult, null, 2)); */
 }
+
 function getHtml() {
 	const hostname = window.location.hostname; // 获取当前主机名
 	const parts = hostname.split('.'); // 按点分割
 	const gatewayDomainName = parts.slice(-2).join('.'); // 返回最后两部分
 	const linktreeUrl = 'https://linktree' + '.' + gatewayDomainName;
 
-    const storageData = localStorage.getItem('data');
-    if(!storageData) {
-        alert("你还没有填写任何数据");
-        return ""
-    }
-	
-	let data = JSON.parse(storageData);	
+	const storageData = localStorage.getItem('data');
+	if (!storageData) {
+		alert('你还没有填写任何数据');
+		return '';
+	}
+
+	const data = JSON.parse(storageData);
 	let result = `<!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Link Tree AR</title>
-    <meta name="description" content="link tree ar" />
+    <title>`;
+	result += data.title;
+	result += `</title>
+    <meta name="description" content="`;
+	result += data.description;
+	result += `" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="`;
 	result += linktreeUrl;
@@ -110,37 +95,39 @@ function getHtml() {
     <div class="container">
         <div class="row">
             <div class="column" style="margin-top: 10%">
-                <img src="https://arweave.net/8MfM94Fd7MRBeQ9-265gGL-EgqMXE6OINSZx5bAu780" style="width:120px;height: 120px;" />
-                <h1 role="heading">Link Tree AR</h1>
-                <p>Link Tree On Permaweb.</p>
-      <a class="button button-default" href="#" target="_blank" rel="noopener" role="button"
-                    ><img
-                        class="icon"
-                        aria-hidden="true"
-                        src="images/icon/generic-website.svg"
-                        alt="website"
-                    />Website</a
-                ><br />
-                <a class="button button-github" href="#" target="_blank" rel="noopener" role="button"
-                    ><img
-                        class="icon"
-                        aria-hidden="true"
-                        src="images/icon/github.svg"
-                        alt="GitHub Logo"
-                    />GitHub</a
-                ><br />
-                <a class="button button-x" href="#" target="_blank" rel="noopener" role="button"
-                    ><img class="icon" aria-hidden="true" src="images/icon/x.svg" alt="X Logo" />Follow on
-                    X</a
-                ><br />
-                <a class="button button-yt" href="#" target="_blank" rel="noopener" role="button"
-                    ><img
-                        class="icon"
-                        aria-hidden="true"
-                        src="images/icon/youtube.svg"
-                        alt="YouTube Logo"
-                    />YouTube</a
-                ><br />
+                <img src="`;
+	result += data.logo;
+	result += `" style="width:120px;height: 120px;" />
+                <h1 role="heading">`;
+	result += data.title;
+	result += `</h1>
+                <p>`;
+	result += data.description;
+	result += `</p>`;
+
+	for (const link in data.links) {
+		result += `<a class="button button-`;
+		result += link.buttonClass;
+		result += `" href="`;
+		result += link.url;
+		result += `" target="_blank" rel="noopener" role="button"
+        ><img
+            class="icon"
+            aria-hidden="true"
+            src="`;
+		result += link.src;
+		result += `"
+            alt="`;
+		result += link.text;
+		result += `"
+        />`;
+		result += `</a
+    >
+    <br />
+    `;
+	}
+
+	result += `      
             </div>
         </div>
     </div>
