d7af829ced9a79feafbb0666259c944ff2ff7012
Eureka Chen <eureka@ee-studio.com>
2024-11-05 09:58:59 +0800

    不发布lua

--- lua/Variable.lua
+++
@@ -1,5 +0,0 @@
-AntProcessId="gJKH_MlxgDI3j912HdppmuJnqzsSvo3nRuvb5PVPxOk"
-
--- Undernames格式为 {{undername="demo",target="xxxxxx"},{undername="eureka",target="xxxxxx"}}
-Undernames = Undernames or {}
-
