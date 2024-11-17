317b85bfe09cbac21dedf60e02700b0705a3be8f
Eureka Chen <eureka@ee-studio.com>
2024-11-17 19:23:34 +0800

    删除upload中无用代码

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,26 +1,20 @@
-//import { TurboFactory, developmentTurboConfiguration } from "@ardrive/turbo-sdk/web";
 import Arweave from "arweave";
 import { prepare } from "./prepare";
+import { log } from "./store/Debug";
 
 export async function upload() {
     const fileContent = prepare();
-    //console.log(fileContent);
-    //demo_linktree.io:4zxHDSCFspfjijZy3XY6QMr28LKEgqICwv7iw-zzR3Y
     try {
-        //const Arweave =await import("arweave");
-        //const arweave = Arweave.init({});
         const arweave = new Arweave({});
         const jwk = await arweave.wallets.generate();
-
         const { TurboFactory, developmentTurboConfiguration } = await import("@ardrive/turbo-sdk/web");
-
         const turboAuthClient = TurboFactory.authenticated({
             privateKey: jwk,
             ...developmentTurboConfiguration
         });
-        console.log("将linktree html文件发布到Turbo服务中...");
 
-        // 创建一个可读流      
+        log("将linktree html文件发布到Turbo服务中...");
+        // 创建一个可读流
         const content = new TextEncoder().encode(fileContent);
         const fileSize = content.length;
         const readableStream = new ReadableStream({
@@ -30,6 +24,7 @@ export async function upload() {
             }
         });
 
+        //上传到Turbo
         const uploadResult = await turboAuthClient.uploadFile({
             fileStreamFactory: () => readableStream,
             fileSizeFactory: () => fileSize,
@@ -46,11 +41,11 @@ export async function upload() {
             signal: AbortSignal.timeout(10_000) // 10秒后取消上传
         });
 
-        console.log("返回结果：", uploadResult);
+        log("上传Turbo返回结果：", uploadResult);
         //获取ID
         return uploadResult.id;
     } catch (error) {
-        console.log("上传到Arweave出错:", error);
+        log("上传到Arweave时出错:", error);
         return "failed";
     }
 }
